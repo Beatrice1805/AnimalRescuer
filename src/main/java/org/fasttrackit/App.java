27ccdcbd34aa67bd5ce253076c362288eb5960b0
Beatrice <oprea.beatrice@yahoo.com>
2020-02-09 10:40:54 +0200

    modified homework

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -4,23 +4,22 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Animal Rescuer Game");
-    }
 
-    PetFood petFood = new PetFood();{
-        petFood.foodLocalDate = LocalDateTime.of(2030, Month.JANUARY, 1, 10,
-                10, 30);
+            Animal animal = new Animal();{
+                animal.name = "Lucky";
+                animal.age = 5;
+                animal.favoriteFoodName = "Royal";
+                System.out.println("Hello,! i'm Lucky , your new friend!");
+            }
 
+            PetFood petFood = new PetFood();
+            {
+                petFood.foodLocalDate = LocalDateTime.of(2030, Month.JANUARY, 1, 10,
+                        10, 30);
+                System.out.println("My favorite food is delicious!");
+            }
+        }
     }
-    Animal animal = new Animal();{
-        animal.name = "Lucky";
-        animal.age = 5;
-        animal.favoriteFoodName = "Royal";
-    }
-}
